69f264fd00ae77a06e6d4d6b7904a8b83d0351f0
masterimpaler <loughlinjon@gmail.com>
2017-05-16 14:11:28 +0300

    fixed jquery problems

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -48,23 +48,32 @@
     </div>
 @endsection
 
-@section('scripts')
+{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>--}}
+{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
+
+@section('script')
+    $('#search').on('keyup', function () {
+    $value = $(this).val();
+    $.ajax({
+    type : 'get',
+    url  : '{{URL::to('search') }}',
+    data : {'search':$value},
+    success:function (data) {
+    $('tbody').html(data);
+    }
+
+
+
+    })
+    })
+@endsection
+{{--<script type="text/javascript">--}}
+    {{----}}
+{{--</script>--}}
+{{--@section('scripts')--}}
 
     {{--<script type="text/javascript">--}}
-        $('#search').on('keyup', function () {
-            $value = $(this).val();
-            $.ajax({
-                type : 'get',
-                url  : '{{URL::to('search') }}',
-                data : {'search':$value},
-                success:function (data) {
-                     $('tbody').html(data);
-                }
 
-
-
-            })
-        })
     {{--</script>--}}
 
-@endsection
+{{--@endsection--}}
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@
 
 Auth::routes();
 
+//Route::get('/logout', 'Auth\LoginController@logout');
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'SearchController@index');
